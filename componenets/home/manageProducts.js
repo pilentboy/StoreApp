5fee04595d81583dc2f33f63a03bb5cfc9d35c2f
pilentboy <sirxmahdi79@gmail.com>
2024-01-28 22:48:58 +0330

    test add new product done

--- componenets/home/manageProducts.js
+++ componenets/home/manageProducts.js
@@ -11,7 +11,7 @@ import AddNewProduct from '../modals/addNewProduct';
 function ManageProducts() {
 
     const [storeChartDisplay, setStoreChartDisplay] = useState(false)
-    const [addNewProductDisplay, setAddNewProductDisplay] = useState(false)
+    const [addNewProductDisplay, setAddNewProductDisplay] = useState(true)
 
     const { fetchData } = useContext(ProductContext)
     return (
--- componenets/modals/addNewProduct.js
+++ componenets/modals/addNewProduct.js
@@ -1,27 +1,68 @@
-import { Modal, StyleSheet, Text, View, Pressable} from "react-native"
+import { Modal, StyleSheet, Text, View, Pressable, ScrollView, TextInput } from "react-native"
 
 import ButtonContainer from "../productDetails/buttonContainer"
 import CloseBTN from "../product/closeBTN"
 import { MaterialIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import Input from "../product/input";
+import { useState, useContext } from "react";
+import { ProductContext } from "../../context/productContext";
 
 function AddNewProduct({ addNewProductDisplay, setAddNewProductDisplay }) {
 
+    const { products, updateProducts } = useContext(ProductContext)
+
+    function AddProduct() {
+
+        const myProducts = products
+        
+        // get the last product id for using a uniqe id for each products
+        const lastProductID = myProducts.slice(-1)[0].id
+
+        const newProduct = { id: lastProductID + 1, name: productName, price: productPrice }
+
+        const updatedProducts = [...products]
+
+        updatedProducts.push(newProduct)
+
+        updateProducts(updatedProducts)
+
+        setAddNewProductDisplay(false)
+    }
+
+    const [productName, setProductName] = useState('')
+    const [productPrice, setProductPrice] = useState('')
+
     return (
+
+
         <Modal visible={addNewProductDisplay} transparent={false} >
 
+            <ScrollView contentContainerStyle={{ flex: 1 }}>
 
 
-            <ButtonContainer>
-                {/* cancle  */}
-                <CloseBTN action={() => setAddNewProductDisplay(false)} />
+                <View style={style.container}>
 
-                {/* apply new info */}
-                <Pressable onPress={() => setAddNewProductDisplay(false)} style={style.btn}>
-                    <AntDesign name="checksquare" size={35} color="green" />
-                </Pressable>
-            </ButtonContainer>
+                    <Input inputValue={productName} inputType='text' title="Name" setNewValue={setProductName} />
+                    <Input inputValue={productPrice} inputType='numeric' title="Price" setNewValue={setProductPrice} />
+
+
+                    <ButtonContainer>
+                        {/* cancle  */}
+                        <CloseBTN action={() => setAddNewProductDisplay(false)} />
+
+                        {/* apply new info */}
+                        <Pressable onPress={AddProduct} style={style.btn}>
+                            <AntDesign name="checksquare" size={35} color="green" />
+                        </Pressable>
+                    </ButtonContainer>
+
+
+                </View>
+
+            </ScrollView>
+
         </Modal>
     )
 
@@ -33,6 +74,13 @@ const style = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
         marginHorizontal: 10
+    },
+    container: {
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+        flex: 1,
+        paddingHorizontal: 20,
     },
 })
 
